2ee3da5c49d692ccef1afbef6bfc8b724c12cfac
Andre van Tonder <drevan@hey.com>
2022-01-16 16:01:02 +1000

    Remove 3rd verse from page 5

--- pages/5-the-result-of-salvation.vue
+++ pages/5-the-result-of-salvation.vue
@@ -1,7 +1,7 @@
 <script setup>
 setPageConfig({
   nextPage: ``,
-  previousPage: `/4`,
+  previousPage: `/4-the-sinners-response`,
 })
 </script>
 
@@ -16,12 +16,6 @@ setPageConfig({
       <BibleVerse reference="Romans 8:1"
         >There is therefore now no condemnation for those who are in Christ
         Jesus.</BibleVerse
-      >
-      <BibleVerse reference="Romans 8:38-39"
-        >For I am sure that neither death nor life, nor angels nor rulers, nor
-        things present nor things to come, nor powers, nor height nor depth, nor
-        anything else in all creation, will be able to separate us from the love
-        of God in Christ Jesus our Lord.</BibleVerse
       >
     </div>
   </div>
